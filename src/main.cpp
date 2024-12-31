bd3a1338bfe88a4320eff82f2d74d8b0cee9bcd3
Daniel (legion) <elmokono@hotmail.com>
2024-12-31 20:11:42 -0300

    added brake flag 2

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ CNS = Custom PIN on RP2040
 */
 
 #define GAS_PIN 26
+#define BRAKE_PIN 12
 #define STEER_PIN 27
 #define LEDS_PIN 1
 #define CE_PIN 22
@@ -24,6 +25,10 @@ CNS = Custom PIN on RP2040
 //pico 19 mosi
 //pico 18 clock
 //pico 16 miso (not used)
+
+#define NULL_FLAG 0
+#define BRAKE_FLAG 2
+#define LEDS_FLAG 3
 
 RF24 radio(CE_PIN, CS_PIN); // CE, CSN
 const byte address[6] = "00010";
@@ -56,7 +61,8 @@ void setup()
 
   //pinMode(STEER_PIN, INPUT_PULLUP);
   //pinMode(GAS_PIN, INPUT_PULLUP);
-  pinMode(LEDS_PIN, INPUT_PULLUP);  
+  pinMode(LEDS_PIN, INPUT_PULLUP);
+  pinMode(BRAKE_PIN, INPUT_PULLUP);
 
   Serial.println("controller init ok");
 }
@@ -79,12 +85,13 @@ void loop()
     maxSteer = steer;
 
   PinStatus leds = digitalRead(LEDS_PIN);
+  PinStatus brake = digitalRead(BRAKE_PIN);
 
   uint8_t buffer[4];
   buffer[0] = 0x0;
   buffer[1] = static_cast<uint8_t>(map(gas, minGas, maxGas, 255, 0)); //long to uint8_t
   buffer[2] = static_cast<uint8_t>(map(steer, minSteer, maxSteer, 0, 255)); //long to uint8_t
-  buffer[3] = leds == LOW ? 0 : 255;
+  buffer[3] = (brake == HIGH) ? NULL_FLAG : BRAKE_FLAG;
 
   Serial.print("command sent ");
   Serial.print(buffer[0]);Serial.print(" ");
